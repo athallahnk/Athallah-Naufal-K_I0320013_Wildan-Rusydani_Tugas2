c4bbbce3039634a222c1f330fcb0fbdd086d9828
athallahnk <80144791+athallahnk@users.noreply.github.com>
2021-03-12 17:39:58 +0700

    Add files via upload

---
+++ I0320013_soal2_tugas2.py
@@ -0,0 +1,37 @@
+#menampilkan perkenalan awal
+print("Namaku adalah Athallah Naufal Kur'anityo, aku biasa dipanggil Athan.")
+print("Nama panggilanku ini merupakan saran dari teman ibuku.")
+#input tanggal lahir dan tahun lahir
+Tanggal_lahir = int(15 * 2 - 13)
+Tahun_lahir = int((200*8)+ 405.05 - 2.1)
+#input umur
+tahun_umur = int(72 / 4)
+bulan_umur = int(2 ** 3)
+semua_bulan = float(18.8*12)
+print("Aku lahir di Surakarta pada tanggal", str(Tanggal_lahir) , "Juli" , str(Tahun_lahir)+".")
+print("Umurku saat ini yaitu", tahun_umur, "tahun lebih", bulan_umur, "bulan atau kisaran", semua_bulan, "bulan.")
+print("Aku tinggal di Surakarta bersama ibu dan kakakku, sedangkan ayahku tinggal di Semarang bersama nenekku.")
+#input alamat
+No = 18
+R_T = 3
+R_W = 13
+print("Alamat rumahku yaitu Jalan Teratai 6 No", No, "RT", R_T, "RW", R_W, "Mangkubumen Wetan Surakarta.")
+#input perawakan tubuh
+tinggi_badan = float((12 ** 2) + (23.5 + 5.7))
+berat_badan = int((100+16) / 2)
+print("Aku memiliki hobi berenang sejak kecil, sehingga aku termasuk orang yang tinggi seumuranku.")
+print("Tinggiku sekarang adalah", tinggi_badan, "cm.", "Namun, karena pandemi yang sedang melanda,")
+print("aku sudah jarang berenang. Aku lebih sering bermain dengan HP dan laptopku baik untuk urusan kuliah maupun lainnya.")
+print("Hal itu menyebabkan berat badanku bertambah menjadi", berat_badan, "kg dan perutku terlihat membesar sedikit")
+#input ukuran outfit
+ukuran_baju = "L"
+ukuran_celana = 32
+ukuran_sepatu = 43
+print("Walaupun badanku lebih besar dari sebelumnya, ukuran bajuku masi tetap sama yaitu", ukuran_baju, "(large).")
+print("Sedangkan ukuran celanaku bertambah 1 cm, yaitu", ukuran_celana, "cm. Karena biasanya ukuran celana itu ganjil,")
+print("agak susah bagiku mencari ukuran celana yang pas, kadang terlalu besar dan kadang terlalu kecil.")
+print("Berikutnya adalah ukuran sepatu, berat dan tinggi badan tidak terlalu berpengaruh terhadap ukuran sepatuku.")
+print("Ukuran sepatuku saat ini adalah", ukuran_sepatu, "dan itu berlangsung selama 5 tahun. ")
+print("Di keluargaku, aku termasuk yang paling tinggi dan itu membuatku menjadi tonggak terdepan dalam urusan")
+print("mengambil sesuatu di tempat tinggi seperti pohon dan lainnya. Aku bersyukur hidup di keluarga ini ")
+print("dan semoga aku, keluargaku, dan siapapun orang baik di dunia ini, akan mendapat balasan terbaik di sisi Tuhan mereka")
